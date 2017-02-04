8e18576baf12624444498ddbbb3525380d25e641
Andrew Jung <andrew-jung@users.noreply.github.com>
2017-02-03 23:37:30 -0400

    Completed HappyNumber, added two WIP

---
+++ RansomNote.java
@@ -0,0 +1,15 @@
+/* LeetCode #383: Ransom Note
+Given an arbitrary ransom note string and another string containing letters from all the magazines,
+write a function that will return true if the ransom note can be constructed from the magazines ;
+otherwise, it will return false.
+Each letter in the magazine string can only be used once in your ransom note.
+*/
+public class RansomNote {
+    public boolean canConstruct(String ransomNote,String magazine){
+        return true;
+    }
+
+    public static void main (String[] args){
+
+    }
+}
